9ae2e9d0fbf1aa7bb1a6356aeacb35e128db2698
Andre Maurell <amaurell@gmail.com>
2023-02-28 03:53:54 -0300

    acerto casas decimais

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ fetch(`https://economia.awesomeapi.com.br/json/last/USD-BRL`).then(resposta=>{
   return resposta.json()
 }).then(corpo=>{
   console.log(corpo)//visualiza as outras opçoes da cotação do dolar
-  document.getElementById("dolar").innerHTML=corpo.USDBRL.bid; 
-  
+  let tmp = Number(document.getElementById("dolar").innerHTML=corpo.USDBRL.bid).toFixed(2); 
+  document.getElementById("dolar").innerHTML=tmp;
+  // let valorDolar = (Number(tmp).toFixed(2))
+  // console.log(tmp)
 
 })
